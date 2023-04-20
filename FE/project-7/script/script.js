c6ae16d388bf22ebd0444dc267ac96965b657414
ikar-zindo <plag.ikar@gmail.com>
2023-04-20 15:17:26 +0200

    project-7

---
+++ FE/project-7/script/script.js
@@ -0,0 +1,61 @@
+//* Однострочный коментарий
+
+/* 
+   *ctrl + /
+   *многострочный коментарий
+*/
+
+//! Команды
+//* alert("Hello, World") //уведосление
+//* console.log("Hello") // отображение в консоли
+//* prompt("Как вас зову?") //ввод данных
+
+//! Переменные
+//* const username = prompt("Как вас зову?")
+//* console.log(username)
+
+//* const userage = prompt("Сколько вам лет?")
+//* console.log(userage)
+
+//TODO: exercise
+// console.log("Hello")
+// console.log("My name Ivan")
+// console.log("I am 29")
+
+// alert("Выберите какой-то из вариантов")
+// alert("Заполните все поля формы")
+
+// prompt("Сколько вам лет?")
+
+// let number = 50
+// number = 100
+// console.log(number)
+
+// const number1 = 50
+// number1 = 100
+// console.log(number1)
+
+//! Конкатенация
+//const userName = prompt("Как вас зовут?")
+//* console.log("Привет, " + userName)
+
+//TODO: exercise 2
+//const userAge = prompt("Сколько тебе лет?")
+// console.log("Привет, " + userName + ". Вам " + userAge + " лет.")
+
+//! Итерполяция
+//console.log(`Привет, ${userName}. Вам ${userAge} лет.`)
+
+//TODO: exercise 3
+// let userAge = prompt("Скалько вам лет?")
+// let birthYears = 2023 - userAge
+// console.log(`Вы роделись в ${birthYears} году.`)
+
+const r = 153
+const g = 153
+const b  = 153
+
+rgb({ r }, { g }, { b })
+
+https://doka.guide/js/var-let/
+
