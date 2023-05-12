9f576786f92b22bb1790d3991bebf1d80cacec2e
ikar-zindo <plag.ikar@gmail.com>
2023-05-12 11:57:45 +0200

    Summary session: Basic FE 12_05_23

--- FE/my-projects/work/12_05_2023/script/script.js
+++ FE/my-projects/work/12_05_2023/script/script.js
@@ -3,21 +3,29 @@ const count_score = document.querySelector("#count_score");
 
 function start_game() {
    object.classList.toggle('start');
-   score = 0;
+   score = 0; // обнуление очков
    count_score.innerText = `0`;
+}
+
+function hit() { // дойствие при попадании
+   score++; 
+   count_score.innerText = `${score}`; // Счёичик очков
+
+   object.classList.remove('start');
+   void object.offsetWidth; // перекомпоновка DOM (API)
+   object.classList.add('start');
+
+   // Случайное появление Луны
+   let random_offset = Math.floor(Math.random() * 400);
+   object.style.left = `${random_offset}px`;
 
 }
 
-function hit(event) {
-   score++;
-   count_score.innerText = `${score}`;
-}
-
-function miss(event) {
+function miss(event) { // дойствие при промахивание
    if (event.target.id == "area") {
       console.log("miss");
    }
 }
 
-let score = 0;
-let object = document.querySelector('#object');
+let score = 0; // очки
+let object = document.querySelector('#object'); // Ищем объект Луна
