e5be0cb1bdaa09fc771ec1e7d38691b367beeabc
ikar-zindo <plag.ikar@gmail.com>
2023-05-12 10:10:16 +0200

    Summary session: Basic FE 12_05_23

---
+++ FE/my-projects/work/12_05_2023/script/script.js
@@ -0,0 +1,4 @@
+const button = document.querySelector("#btm"); // Ищем btm
+const main = document.querySelector("main"); // Ищем main
+const section = document.querySelector("section");
+const btm = document.querySelector(".btm-add-all");
