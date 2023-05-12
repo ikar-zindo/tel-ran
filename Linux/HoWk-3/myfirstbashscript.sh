e44e2296a19058f08b19ac6b65b78aed4bdd0c96
ikar-zindo <plag.ikar@gmail.com>
2023-05-12 22:51:49 +0200

    My first bash script

---
+++ Linux/HoWk-3/myfirstbashscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Hello, $USER!"
+pwd
+ps -ef | wc -l
+
+#cd /opt/280323_morning/ikar-zindo/
+#nano myfirstbashscript.sh 
+#chmod +x myfirstbashscript.sh
+#export USER=Ivan_Bukrieiev
