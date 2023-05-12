694c3112ec11e7a2d6b633525daedc3913d03874
ikar-zindo <plag.ikar@gmail.com>
2023-05-12 22:19:57 +0200

    loop sound

--- FE/my-projects/work/12_05_2023/script/script.js
+++ FE/my-projects/work/12_05_2023/script/script.js
@@ -1,7 +1,7 @@
 
-const count_score = document.querySelector("#count_score"); 
-const area = document.querySelector('#area');
-const gameOver = document.querySelector(".game-over");
+const count_score = document.querySelector("#count_score");  // Ищем объект Луна
+const area = document.querySelector('#area'); // Ищем объект игровое поле
+const gameOver = document.querySelector(".game-over") // Ищем сообшение Game Over
 
 let score = 0; // очки
 let object = document.querySelector('#object'); // Ищем объект Луна
@@ -9,6 +9,13 @@ const hit_sound = new Audio('sounds/hit.wav'); // Звук
 const miss_sound = new Audio('sounds/miss.wav'); // Звук
 const game_level_music = new Audio('sounds/level-music.wav'); // Звук
 
+// Зацикливаем мелодию уровня
+setInterval(function(){
+   game_level_music.currentTime = 0; // этой строки может не быть, если время звучания аудиофайла меньше, чем 15 с.
+   game_level_music.play();
+}, 30000);
+
+// Старт
 function start_game() {
    area.classList.remove('finish');
    object.classList.toggle('start');
@@ -36,7 +43,7 @@ function hit() { // дойствие при попадании
 }
 
 function miss(event) { // дойствие при промахивание
-   if (event.target.id == "area") {
+   if (event.target.id == "area" || event.target.id == "wrapper-game-over" ) {
       score--; 
       count_score.innerText = `${score}`;
          if (score < 0) {
@@ -45,15 +52,12 @@ function miss(event) { // дойствие при промахивание
       miss_sound.currentTime = 0; // Звук
       miss_sound.play();    // Звук
    }
-
 }
 
 function finish_game() {
    const gameOver = document.querySelector(".game-over");
-   area.classList.add('finish');
    object.classList.remove('start');
    count_score.innerText = `0`;
-
    gameOver.style.display = "block";
 
    game_level_music.pause(); // Звук
