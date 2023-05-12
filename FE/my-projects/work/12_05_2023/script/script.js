4e6d808bd56efcbb54d337507c2680e08988f696
ikar-zindo <plag.ikar@gmail.com>
2023-05-12 11:26:05 +0200

    Summary session: Basic FE 12_05_23

--- FE/my-projects/work/12_05_2023/script/script.js
+++ FE/my-projects/work/12_05_2023/script/script.js
@@ -1,4 +1,20 @@
-const button = document.querySelector("#btm"); // Ищем btm
-const main = document.querySelector("main"); // Ищем main
-const section = document.querySelector("section");
-const btm = document.querySelector(".btm-add-all");
+// const count_score = document.querySelector("#count_score");
+
+function start_game() {
+   object.classList.toggle('start');
+}
+
+function hit(event) {
+   const count_score = document.querySelector("#count_score");
+   score++;
+   count_score.innerText = `${score}`;
+}
+
+function miss(event) {
+   if (event.target.id == "area") {
+      console.log("miss");
+   }
+}
+
+let score = 0;
+let object = document.querySelector('#object');
