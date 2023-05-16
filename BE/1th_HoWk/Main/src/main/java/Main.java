0b04dee37041d2e3750e0542ed64d9d3b397d302
ikar-zindo <plag.ikar@gmail.com>
2023-05-16 14:58:24 +0200

    my 1th HoWk Java

---
+++ BE/1th_HoWk/Main/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/*
+   1 уровень сложности: Разработайте алгоритм, используя сканнер,
+   который принимал бы любое целое число и выводил на экран его десятичную,
+   восьмеричную, двоичную и шестнадцатеричную версию
+*/
+public class Main {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in); // Запускаем Scanner
+
+      int num = sc.nextInt(); // Заносим в переменную число из консоли
+
+      System.out.println("Decimal:  " + num); // 10
+      System.out.println("Binary:  " + Integer.toBinaryString(num)); // 10 -> 2
+      System.out.println("Octal:  " + Integer.toOctalString(num)); // 10 -> 8
+      System.out.println("Hexadecimal:  " + Integer.toHexString(num)); // 10 -> 16
+   }
+}
