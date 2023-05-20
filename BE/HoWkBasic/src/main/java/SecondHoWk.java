ccae02ce898292351a9f575f23e6a07c6a9597c8
ikar-zindo <plag.ikar@gmail.com>
2023-05-20 12:49:00 +0200

    upload SS/HW

--- BE/HoWkBasic/src/main/java/SecondHoWk.java
+++ BE/HoWkBasic/src/main/java/SecondHoWk.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class SecondHoWk {
+import java.util.Scanner;
+/*
+   1 уровень сложности: попробуйте реализовать 2 алгоритма,
+которые переводили бы значение типа char (равен '4') в целочисленный тип (например в int).
+Первый с помощью String.valueOf, второй - с помощью Character.getNumericValue.
+Для проверки используйте арифметическую операцию result + 6, чтоб в итоге получить 10
+*/
+
+public class SecondHoWk {
+   public static void main(String[] args) {
+
+   }
 }
