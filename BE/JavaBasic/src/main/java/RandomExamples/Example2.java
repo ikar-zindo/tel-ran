038c76b4ecd0244d168d1dd02ab0159d9a2265ad
ikar-zindo <plag.ikar@gmail.com>
2023-05-26 15:05:56 +0200

    update random example

--- BE/JavaBasic/src/main/java/RandomExamples/Example2.java
+++ BE/JavaBasic/src/main/java/RandomExamples/Example2.java
@@ -1,2 +1,16 @@
-package RandomExamples;public class Example2 {
+package RandomExamples;
+
+public class Example2 {
+   public static void main(String[] args) {
+
+      // примеры на базе Math.random();
+      double q = Math.random(); // [0;1)
+
+      int w = (int) (Math.random() * 100); // [0;99] == [0;100)
+
+      // [3;20]
+      int e = (int) Math.random() * 17 + 3;
+
+      System.out.println(e);
+   }
 }
