e0e0a038965a49b4c79577389a1c6ffdaf4c56ad
ikar-zindo <plag.ikar@gmail.com>
2023-05-30 22:04:29 +0200

    условные/логические/ещё какие-то операторы

--- BE/JavaBasic/src/main/java/First.java
+++ BE/JavaBasic/src/main/java/First.java
@@ -1,7 +1,9 @@
 public class First {
    // main
    // sout
-   //
+   // Ctrl + d   - копия строки
+   // Ctrl + p   - подсветка количества аргументов и их типов (сигнатура метода)
+   // Ctrl + /   - комментирование целой строки
 
 
    public static void main(String[] args) {
---
+++ BE/JavaBasic/src/main/java/IfElseExamples/Example1.java
@@ -0,0 +1,14 @@
+package IfElseExamples;
+
+public class Example1 {
+   public static void main(String[] args) {
+
+      boolean freeTime = false;
+
+      if (freeTime == true) {
+         System.out.println("Идём на ланч");
+      } else {
+         System.out.println("Идём на хуй");
+      }
+   }
+}
