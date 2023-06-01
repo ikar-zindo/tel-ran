0939b51633f16632e3a0925b3724b0ecfa4a946e
ikar-zindo <plag.ikar@gmail.com>
2023-06-01 15:01:46 +0200

    swich_case

--- BE/JavaBasic/src/main/java/SwitchExamples/Example1.java
+++ BE/JavaBasic/src/main/java/SwitchExamples/Example1.java
@@ -1,2 +1,66 @@
-package SwitchExamples;public class Example1 {
+package SwitchExamples;
+
+import java.util.Scanner;
+
+public class Example1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter pls number of day in week [1..7]: ");
+        int day = sc.nextInt();
+        sc.close();
+
+//        if (day == 1) {
+//            System.out.println("MO");
+//        } else if (day == 2) {
+//            System.out.println("TU");
+//        } else if (day == 3) {
+//            System.out.println("WE");
+//        } else if (day == 4) {
+//            System.out.println("TH");
+//        } else if (day == 5) {
+//            System.out.println("FR");
+//        } else if (day == 6) {
+//            System.out.println("SA");
+//        } else if (day == 7) {
+//            System.out.println("SU");
+//        } else {
+//            System.out.println("Error in day");
+//        }
+
+
+        switch (day) {
+
+            case 1:
+                System.out.println("MO");
+                break;
+
+            case 2:
+                System.out.println("TU");
+                break;
+
+            case 3:
+                System.out.println("WE");
+                break;
+
+            case 4:
+                System.out.println("TH");
+                break;
+
+            case 5:
+                System.out.println("FR");
+                break;
+
+            case 6:
+                System.out.println("SA");
+                break;
+
+            case 7:
+                System.out.println("SU");
+                break;
+
+            default:
+                System.out.println("Error in day");
+        }
+    }
 }
--- BE/JavaBasic/src/main/java/SwitchExamples/Example4.java
+++ BE/JavaBasic/src/main/java/SwitchExamples/Example4.java
@@ -1,2 +1,43 @@
-package SwitchExamples;public class Example4 {
+package SwitchExamples;
+
+import java.util.Scanner;
+
+public class Example4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter pls year of education [1..2]: ");
+        int year = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.print("Enter pls your branch [Berlin, Tel-Aviv, New-York]: ");
+        String branch = sc.nextLine();
+        sc.close();
+
+        switch (branch) {
+            case "Berlin":
+                System.out.print("Branch Berlin");
+                break;
+            case "Tel-Aviv":
+                System.out.print("Branch Tel-Aviv");
+                break;
+            case "New-York":
+                System.out.print("Branch New-York");
+                break;
+            default:
+                System.out.print("Select branch is wrong, ");
+        }
+
+        switch (year) {
+            case 1:
+                System.out.print(", year 1.");
+                break;
+            case 2:
+                System.out.print(", year 2.");
+                break;
+            default:
+                System.out.print("year of education is wrong.");
+
+        }
+    }
 }
