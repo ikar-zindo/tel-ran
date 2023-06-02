f839ac77f028d9531bc9109b5fa96fc3dbd586c5
ikar-zindo <plag.ikar@gmail.com>
2023-06-02 11:53:22 +0200

    SS_02_06_23 upload

--- BE/SummarySession/src/main/java/SS_02_06_23/Employee3.java
+++ BE/SummarySession/src/main/java/SS_02_06_23/Employee3.java
@@ -1,17 +1,43 @@
 package SS_02_06_23;
 
 public class Employee3 {
-    private final int baseSalary;
-    private final int extraHours;
-    private final int hourlyRate;
+    private int baseSalary;
+    public int extraHours;
+    public int hourlyRate;
 
-    public Employee3(int baseSalary, int extraHours, int hourlyRate) {
+    public int getBaseSalary(){
+        return baseSalary;
+    }
+
+    public int getExtraHours() {
+        return extraHours;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setBaseSalary(int baseSalary){
+        if (baseSalary<=0){System.out.println("wrong number");
+            return;}
         this.baseSalary = baseSalary;
+    }
+
+    public void setExtraHours(int extraHours) {
+        if (extraHours <= 0) {
+            System.out.println("wrong number");
+        }
         this.extraHours = extraHours;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0) {
+            System.out.println("wrong number");
+        }
         this.hourlyRate = hourlyRate;
     }
 
-    public int calculateTax() {
-        return baseSalary + extraHours * hourlyRate;
+    public int calculateTax(){
+        return baseSalary+extraHours*hourlyRate;
     }
 }
--- BE/SummarySession/src/main/java/SS_02_06_23/Main2.java
+++ BE/SummarySession/src/main/java/SS_02_06_23/Main2.java
@@ -2,8 +2,13 @@ package SS_02_06_23;
 
 public class Main2 {
     public static void main(String[] args) {
-        Employee3 employee = new Employee3(50000, 10, 20);
+        Employee3 employee = new Employee3();
+        employee.setBaseSalary(50000);
+        employee.setExtraHours(20);
+        employee.setHourlyRate(10);
 
-        System.out.println(employee.calculateTax());
+        System.out.println(employee.getBaseSalary());
+        System.out.println(employee.getExtraHours());
+        System.out.println(employee.getHourlyRate());
     }
 }
