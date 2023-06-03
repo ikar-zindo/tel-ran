a775b76ec2e547ebab1f084ce63e183a8344ed97
ikar-zindo <plag.ikar@gmail.com>
2023-06-03 21:12:36 +0200

    exercise 7

--- BE/ExtraHoWk_MikhailEgorov/src/main/java/First.java
+++ BE/ExtraHoWk_MikhailEgorov/src/main/java/First.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /*
@@ -16,7 +17,25 @@ import java.util.List;
 public class First {
    public static void main(String[] args) {
 
-      // Exercise 0
+//      Exercise0();
+
+//      Exercise1();
+
+//      Exercise2();
+
+//      Exercise3();
+
+//      Exercise4();
+
+//      Exercise5();
+
+//      Exercise6();
+
+      Exercise7();
+   }
+
+   // Exercise 0
+   public static void Exercise0() {
       int[] myArray1 = {78, 14, 13, 54, 35, 16, 75}; // массив целых чисел для манипуляций
 
       for (int i = 0; i < myArray1.length; i++) { // цикл для нахождения чётных чисел
@@ -24,15 +43,21 @@ public class First {
             System.out.println(myArray1[i]); // вывод в кончоль чёных числел
          }
       }
+   }
 
-      // Exercise 1
+   // Exercise 1
+   public static void Exercise1() {
+      int[] myArray1 = {78, 14, 13, 54, 35, 16, 75}; // массив целых чисел для манипуляций
+
       int sum = 0; // переменная для подсчёта суммы
       for (int i = 0; i < myArray1.length; i++) { // цикл для подсчёта суммы эл массива
          sum += myArray1[i];
       }
       System.out.println(sum); // вывод суммы в консоль
+   }
 
-      // Exercise 2
+   // Exercise 2
+   public static void Exercise2() {
       String isPalindrome = "Level"; // пороверяемое слово
 
       String isisPalindromeToUpperCase = isPalindrome.toUpperCase(); // переводи в верхний регистр
@@ -43,8 +68,17 @@ public class First {
       } else {
          System.out.println("Слово " + isPalindrome + " НЕ является палиндромом.");
       }
+   }
 
-      // Exercise 3
+   // Метод для Exercise 2
+   // Подключаем StringBuilder для переворачивания строки в обратном порядке
+   // P.S. Я не знаю, как это работает. Но оно днлает то, что мне нужно
+   public static String reverse(String str) {
+      return new StringBuilder(str).reverse().toString();
+   }
+
+   // Exercise 3
+   public static void Exercise3() {
       double[] myArray2 = new double[]{1.2, 2.9, 7}; // Массив double
 
       double average = 0; // переменная среднее арифметическое
@@ -55,8 +89,10 @@ public class First {
       }
       average = sumMyArray2 / myArray2.length; // вычесляем среднее арифметическое
       System.out.println("Среднее арифметическое массива double: " + average); // Выыод значения в консоль
+   }
 
-      // !!! Exercise 4
+   // !!! Exercise 4
+   public static void Exercise4() {
       List<String> st = new ArrayList<>(); // Список строк
       st.add("Ivan");
       st.add("Ivan");
@@ -84,21 +120,53 @@ public class First {
          }
       }
       System.out.println(st);
+   }
 
-      // Exercise 5
-      int[] myArray3 = {78, 14, 13, 54, 35, 16, 75}; // массив целых чисел для манипуляций
+   // Exercise 5
+   public static void Exercise5() {
+      int[] myArray = {78, 14, 13, 54, 35, 16, 75}; // массив целых чисел для сортировки пузырьком
 
-      for (int i = 0; i < myArray3.length; i++) {
-         if (myArray1[i] > myArray3[i + 1]) {
+      System.out.println(Arrays.toString(myArray));
 
+      for (int i = myArray.length - 1; i >= 1 ; i--) {
+         for (int j = 0; j < i; j++) {
+            if (myArray[j] > myArray[j + 1]) {
+               int tmp = myArray[j];
+               myArray[j] = myArray[j + 1];
+               myArray[j + 1] = tmp;
+            }
          }
       }
+      System.out.println(Arrays.toString(myArray));
    }
 
-   // Метод для Exercise 2
-   // Подключаем StringBuilder для переворачивания строки в обратном порядке
-   // P.S. Я не знаю, как это работает. Но оно днлает то, что мне нужно
-   public static String reverse(String str) {
-      return new StringBuilder(str).reverse().toString();
+   // Exercise 6
+   public static void Exercise6() {
+      int[] myArray = {100, 78, 14, 13, 54, 35, 16}; // массив целых чисел для поиска числа
+
+      int max = myArray[0];
+      int min = myArray[0];
+
+      for (int i = 0; i < myArray.length - 1; i++) {
+         if (max < myArray[i + 1]) {
+            max = myArray[i + 1];
+         }
+         if (min > myArray[i + 1]) {
+            min = myArray[i + 1];
+         }
+      }
+
+
+      System.out.println("max = " + max);
+      System.out.println("min = " + min);
    }
+
+   public static void Exercise7() {
+      int[] myArray = {100, 78, 14, 13, 54, 35, 16}; // массив целых чисел для реверса
+
+      /* СДЕСЬ МОГ БЫ БЫТЬ ВАШ КОД */
+
+   }
+
+
 }
