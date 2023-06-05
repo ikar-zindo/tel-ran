9a0c75e26721cb1a32465e6a65df256555933abe
ikar-zindo <plag.ikar@gmail.com>
2023-06-05 13:51:07 +0200

    power of two

---
+++ BE/HoWkBasic/src/main/java/Ninth.java
@@ -0,0 +1,9 @@
+public class Ninth {
+   public static void main(String[] args) {
+
+      for (int i = 0; i < 10; i++) {
+         int tmp = (int) Math.pow(2, i);
+         System.out.print(tmp + " ");
+      }
+   }
+}
