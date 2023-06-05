526c69aaffd5cd7dd0a8363059dd1487bca5582f
ikar-zindo <plag.ikar@gmail.com>
2023-06-05 13:33:29 +0200

    for

---
+++ BE/JavaBasic/src/main/java/Cycles/ForExample2.java
@@ -0,0 +1,10 @@
+package Cycles;
+
+public class ForExample2 {
+   public static void main(String[] args) {
+
+      for (byte i = 0; i < 150; i++) {
+         System.out.println(i);
+      }
+   }
+}
