984394f429dc7c8b819f603a76a67e931da306a8
ikar-zindo <plag.ikar@gmail.com>
2023-06-08 21:32:42 +0200

    10_homework

--- BE/HoWkBasic/src/main/java/Tenth.java
+++ BE/HoWkBasic/src/main/java/Tenth.java
@@ -9,5 +9,28 @@
 H, N, M можно оформить через сканнер. Для теста можно использовать значения, например H = 200, N = 50, M = 1
  */
 
+import java.util.Scanner;
+
 public class Tenth {
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.print("Введите общее количество этажей (h): ");
+      int h = scanner.nextInt();
+
+      System.out.print("Введите количество этажей подъема (n): ");
+      int n = scanner.nextInt();
+
+      System.out.print("Введите количество этажей спуска (m): ");
+      int m = scanner.nextInt();
+
+      int difference = h - n;
+      if (difference >= 0) {
+         System.out.println(1);
+      } else {
+         double fullJourneys = Math.ceil((double) h / (n - m));
+         int totalJourneys = (int) fullJourneys + 1;
+         System.out.println(totalJourneys);
+      }
+   }
 }
