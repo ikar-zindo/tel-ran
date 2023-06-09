e5dd37c07328826385c14a4aae4a1d9537b485e2
ikar-zindo <plag.ikar@gmail.com>
2023-06-09 10:04:40 +0200

    inheritance

--- BE/JavaBasic/src/main/java/Relationships/Example.java
+++ BE/JavaBasic/src/main/java/Relationships/Example.java
@@ -1,101 +1,240 @@
 package Relationships;
 
+
 public class Example {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
-//      Animal a = new Animal();
-////      System.out.println(a.paws);
-//
-//      Dog d = new Dog();
-////      System.out.println(d.paws);
-//      d.bark();
+        Animal a = new Animal();
+        System.out.println(a);
 
-      Barkable fb = new FrBulldog();
-      fb.bark();
+        Dog d = new Dog();
 
-//      GerShepherd gs = new GerShepherd();
-//      gs.bark();
-//
-//      Bird b = new Bird();
-//
-//      Eagle e = new Eagle();
-//      e.fly();
-//
-//      Parrot p = new Parrot();
-//      p.fly();
+        FrBulldog fb = new FrBulldog();
+        System.out.println(fb);
 
-   }
+        GerSheppard gs = new GerSheppard();
+        System.out.println(gs);
+        // возможность скорректировать длину хвоста
+//        gs.getTail().setLength(25);
+//        System.out.println(gs.getTail().getLength());
+
+        Bird b = new Bird();
+
+        Eagle e = new Eagle();
+
+        Parrot p = new Parrot();
+
+        System.out.println();
+
+    }
+}
+
+class Tail {
+    private int length;
+
+    public Tail(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "Tail = " + length + " cm";
+    }
 }
 
 class Animal {
-   public int paws = 4;
-   public int tail = 1;
+    private int paws = 4;
+    private int tails = 1;
+    private Tail tail = new Tail(20);
+
+    public Tail getTail() {
+        return tail;
+    }
+
+    public void setTail(Tail tail) {
+        this.tail = tail;
+    }
+
+    public int getPaws() {
+        return paws;
+    }
+
+    public void setPaws(int paws) {
+        this.paws = paws;
+    }
+
+    public int getTails() {
+        return tails;
+    }
+
+    public void setTails(int tails) {
+        this.tails = tails;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getName() + " with paws = " + paws + ", tails = " + tails;
+    }
 }
 
 class Dog extends Animal implements Barkable {
-
-   @Override
-   public void bark() {
-      System.out.println("Bark, bark!!!");
-   }
+    @Override
+    public void bark() {
+        System.out.println("Bark, bark!!!");
+    }
 }
 
 class FrBulldog extends Dog {
-   public int avgLengthPaws = 15;
-   public int avgLengthTail = 4;
-   public int avgLengthFur = 1;
+    private int avgLengthPaws = 15;
+    private int avgLengthFur = 1;
+    private Tail tail = new Tail(4);
 
-   @Override
-   public void bark() {
-      System.out.println("Bark, french bulldog, bark!!!");
-   }
+    @Override
+    public Tail getTail() {
+        return tail;
+    }
+
+    @Override
+    public void setTail(Tail tail) {
+        this.tail = tail;
+    }
+
+    @Override
+    public void bark() {
+        System.out.println("Bark, french bulldog, bark!!!");
+    }
+
+    public int getAvgLengthPaws() {
+        return avgLengthPaws;
+    }
+
+    public void setAvgLengthPaws(int avgLengthPaws) {
+        this.avgLengthPaws = avgLengthPaws;
+    }
+
+    public int getAvgLengthFur() {
+        return avgLengthFur;
+    }
+
+    public void setAvgLengthFur(int avgLengthFur) {
+        this.avgLengthFur = avgLengthFur;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", " + tail;
+    }
 }
 
-class GerShepherd extends Dog {
-   public int avgLengthPaws = 40;
-   public int avgLengthTail = 25;
-   public int avgLengthFur = 4;
+class GerSheppard extends Dog {
+    private int avgLengthPaws = 40;
+    private int avgLengthFur = 4;
+    private Tail tail = new Tail(30);
 
-   @Override
-   public void bark() {
-      System.out.println("Bark, german shepherd, bark!!!");
-   }
+    @Override
+    public Tail getTail() {
+        return tail;
+    }
+
+    @Override
+    public void setTail(Tail tail) {
+        this.tail = tail;
+    }
+
+    @Override
+    public void bark() {
+        System.out.println("Bark, german sheppard, bark!!!");
+    }
+
+    public int getAvgLengthPaws() {
+        return avgLengthPaws;
+    }
+
+    public void setAvgLengthPaws(int avgLengthPaws) {
+        this.avgLengthPaws = avgLengthPaws;
+    }
+
+    public int getAvgLengthFur() {
+        return avgLengthFur;
+    }
+
+    public void setAvgLengthFur(int avgLengthFur) {
+        this.avgLengthFur = avgLengthFur;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", " + tail;
+    }
 }
 
-// итерфейс для примера
+// интерфейс для примера
 interface A {
-   void preparing();
-   void cocking();
-   void serving();
-   void eating();
+    void preparing();
+    void cocking();
+    void serving();
+    void eating();
 }
 
 interface Barkable {
-   void bark();
+    void bark();
 }
 
 class Bird extends Animal {
-   public int paws = 2;
-   public int winds = 2;
-   public int beak = 1;
+    private int paws = 2;
+    private int winds = 2;
+    private int beak = 1;
+
+    @Override
+    public int getPaws() {
+        return paws;
+    }
+
+    @Override
+    public void setPaws(int paws) {
+        this.paws = paws;
+    }
+
+    public int getWinds() {
+        return winds;
+    }
+
+    public void setWinds(int winds) {
+        this.winds = winds;
+    }
+
+    public int getBeak() {
+        return beak;
+    }
+
+    public void setBeak(int beak) {
+        this.beak = beak;
+    }
 }
 
-class Eagle extends Bird implements Flying{
-
-   @Override
-   public void fly() {
-      System.out.println("Eagle is flying");
-   }
+class Eagle extends Bird implements Flying {
+    @Override
+    public void fly() {
+        System.out.println("Eagle is flying");
+    }
 }
 
 class Parrot extends Bird implements Flying {
 
-   @Override
-   public void fly() {
-      System.out.println("Parrot is flying");
-   }
+    @Override
+    public void fly() {
+        System.out.println("Parrot is flying");
+    }
 }
 
 interface Flying {
-   void fly();
+    void fly();
 }
--- BE/JavaBasic/src/main/java/Relationships/Example5.java
+++ BE/JavaBasic/src/main/java/Relationships/Example5.java
@@ -1,2 +1,21 @@
-package Relationships;public class Example5 {
+package Relationships;
+
+// примеры анонимных вложенных классов
+public class Example5 {
+
+   public static void main(String[] args) {
+
+      TVable tv = new TVable() {
+         @Override
+         public void show() {
+            System.out.println("Show must go on");
+         }
+      };
+
+      tv.show();
+   }
+}
+
+interface TVable {
+   void show();
 }
