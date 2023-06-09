78b67523801f6a26da65938f92db3c73a6909bc7
ikar-zindo <plag.ikar@gmail.com>
2023-06-09 10:04:40 +0200

    inheritance

--- BE/JavaBasic/src/main/java/Relationships/Example3.java
+++ BE/JavaBasic/src/main/java/Relationships/Example3.java
@@ -1,2 +1,49 @@
-package Relationships;public class Example3 {
+package Relationships;
+
+// пример локального внутреннего класса метода
+public class Example3 {
+
+
+
+   // круиз с шоу раз в Х дней
+   public void startCruise() {
+
+      class Show {
+
+         public Show() {
+            System.out.println("Show was started");
+         }
+
+         private int peopleCounts = 50;
+         private String nameOfShow = "Carnival";
+
+         public int getPeopleCounts() {
+            return peopleCounts;
+         }
+
+         public void setPeopleCounts(int peopleCounts) {
+            this.peopleCounts = peopleCounts;
+         }
+
+         public String getNameOfShow() {
+            return nameOfShow;
+         }
+
+         public void setNameOfShow(String nameOfShow) {
+            this.nameOfShow = nameOfShow;
+         }
+      }
+
+      Show s = new Show();
+
+      System.out.println(s.getNameOfShow());
+      System.out.println(s.getPeopleCounts());
+   }
+
+
+   public static void main(String[] args) {
+
+      Example3 e3 = new Example3();
+      e3.startCruise();
+   }
 }
--- BE/JavaBasic/src/main/java/Relationships/Example4.java
+++ BE/JavaBasic/src/main/java/Relationships/Example4.java
@@ -1,2 +1,21 @@
-package Relationships;public class Example4 {
+package Relationships;
+
+// пример статического вложеного класса
+public class Example4 {
+
+   public static void tstMethod() {
+      System.out.println("Test method is correct");
+   }
+
+   static class Inner2 {
+      public static void printMethodInner2() {
+         System.out.println("Method inside static class is correct");
+      }
+   }
+
+   public static void main(String[] args) {
+      tstMethod();
+      Inner2.printMethodInner2();
+
+   }
 }
