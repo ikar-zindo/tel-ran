9808998372f4c615cfa4f75fc529ddb068d39b3c
ikar-zindo <plag.ikar@gmail.com>
2023-06-12 13:40:04 +0200

    Days of week

---
+++ BE/HoWkBasic/src/main/java/Twelfth.java
@@ -0,0 +1,16 @@
+public class Twelfth {
+   public static void main(String[] args) {
+
+      String[] daysOfWeek = new String[7];
+
+      daysOfWeek[0] = "Monday";
+      daysOfWeek[1] = "Tuesday";
+      daysOfWeek[2] = "Wednesday";
+      daysOfWeek[3] = "Thursday";
+      daysOfWeek[4] = "Friday";
+      daysOfWeek[5] = "Saturday";
+      daysOfWeek[6] = "Sunday";
+
+      System.out.println(daysOfWeek[daysOfWeek.length - 1]);
+   }
+}
