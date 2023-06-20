a20d0cecfd433db488d69a39588ffdf774e12329
ikar-zindo <plag.ikar@gmail.com>
2023-06-20 16:30:32 +0200

    upload lessons 1

---
+++ BE/JavaPro/src/main/java/lesson1/task1/Dog.java
@@ -0,0 +1,43 @@
+package lesson1.task1;
+
+import java.util.Date;
+
+public class Dog {
+
+   int age;
+   String color;
+   Date date;
+
+   // Конструктор по умолчанию, который будет добавлен в наш класс
+   // автоматически компилятором, если мы его не напишем сами.
+   public Dog() {
+      System.out.println("Вызван конструктор класса Dog.");
+   }
+
+   // Конструктор с входящим параметром
+   // Создаёт собаку сразу с каким-то определённым возрастом
+   public Dog(int age) {
+      this.age = age;
+   }
+
+   public Dog(String color) {
+      this.color = color;
+   }
+
+   public Dog(int age, String color) {
+      this.age = age;
+      this.color = color;
+   }
+
+   public void sleep() {
+      System.out.println("Собака спит");
+   }
+
+   public void bark() {
+      System.out.println("Гав-Гав!");
+   }
+
+   public void test() {
+      sleep();
+   }
+}
---
+++ BE/JavaPro/src/main/java/lesson1/task1/Main.java
@@ -0,0 +1,37 @@
+package lesson1.task1;
+
+public class Main {
+
+   public static void main(String[] args) {
+
+      // 1. Создание переменной (объект ещё не создаётся)
+      Dog dog;
+
+      // 2. Создаётся объект собаки, под него выделяется память,
+      // и ссылка на этот объект записывается в переменную dog
+      System.out.println("Создаём объект собаки.");
+      dog = new Dog();
+      System.out.println("Объект собаки создан.");
+
+      dog.age = 7;
+      dog.color = "BLACK";
+
+      System.out.println("Возраст - " + dog.age);
+      System.out.println("Цвет - " + dog.color);
+
+      dog.bark();
+      dog.sleep();
+
+      Dog secondDog = new Dog(5);
+      System.out.println("Возраст второй собаки - " + secondDog.age);
+
+      Dog thirdDog = new Dog(9);
+      System.out.println("Возраст третьей собаки - " + thirdDog.age);
+
+      Dog fourthDog = new Dog(3, "WHITE");
+
+      secondDog.sleep();
+      thirdDog.sleep();
+      fourthDog.bark();
+   }
+}
