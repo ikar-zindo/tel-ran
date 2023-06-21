06e70b77b18da8a002104ea2a6682cc55d6270aa
ikar-zindo <plag.ikar@gmail.com>
2023-06-21 19:47:58 +0200

    homework_1

---
+++ BE/HoWkPro/src/main/java/HoWk1/PersonMain.java
@@ -0,0 +1,9 @@
+package HoWk1;
+
+public class PersonMain {
+   public static void main(String[] args) {
+
+      Person person1 = new Person();
+      Person person2 = new Person("Ivan", 198);
+   }
+}
