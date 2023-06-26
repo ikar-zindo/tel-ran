47a5dc7e91cfe803bb1e39a67678d89b8a3c4fcf
ikar-zindo <plag.ikar@gmail.com>
2023-06-26 10:15:00 +0200

    update lesson 3

---
+++ BE/JavaPro/src/main/java/lesson3/task1/Document.java
@@ -0,0 +1,14 @@
+package lesson3.task1;
+
+public class Document {
+
+   private String status;
+
+   public Document(String status) {
+      this.status = status;
+   }
+
+   public String getStatus() {
+      return status;
+   }
+}
