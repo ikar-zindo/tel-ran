20dbffa5d3a0b7da1c3cd3e2379ec4840eb4a0ed
ikar-zindo <plag.ikar@gmail.com>
2023-06-27 15:30:23 +0200

    java_pro_enum

--- BE/JavaPro/src/main/java/lesson3/task3/Mian.java
+++ BE/JavaPro/src/main/java/lesson3/task3/Mian.java
@@ -5,12 +5,25 @@ public class Mian {
 
       Document document = Document.SENT;
 
-      if (document.equals(Document.SENT)) {
-         System.out.println("Документ отправлен");
-      } else if (document.equals(Document.DELIVERED)) {
-         System.out.println("Документ доставлен");
-      } else if (document.equals(document.READ)) {
-         System.out.println("Документ прочитан");
+//      if (document.equals(Document.SENT)) {
+//         System.out.println("Документ отправлен");
+//      } else if (document.equals(Document.DELIVERED)) {
+//         System.out.println("Документ доставлен");
+//      } else if (document.equals(document.READ)) {
+//         System.out.println("Документ прочитан");
+//      }
+
+      switch (document) {
+         case SENT -> System.out.println("Документ отправлен");
+
+         case DELIVERED -> System.out.println("Документ доставлен");
+
+         case READ -> System.out.println("Документ прочитан");
+
+         case DELETED -> System.out.println("Документ удалён");
       }
+
+      System.out.println("Работа метода toString()");
+      System.out.println(document);
    }
 }
---
+++ BE/JavaPro/src/main/java/lesson3/task4/Main.java
@@ -0,0 +1,30 @@
+package lesson3.task4;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+public class Main {
+   public static void main(String[] args) {
+
+      Day day = Day.THURSDAY;
+
+      int order = day.ordinal();
+      System.out.println("Order: " + order);
+
+      Day day1 = getDayOfWeek("Friday");
+      System.out.println(day1.ordinal());
+
+      Day[] days = Day.values();
+      System.out.println("Названия дней недели");
+      System.out.println(Arrays.toString(days));
+
+      for (Day currentDay : days) {
+         System.out.print(currentDay + " ");
+      }
+   }
+
+   public static Day getDayOfWeek(String nameOfDay) {
+      Day day = Day.valueOf(nameOfDay.toUpperCase());
+      return day;
+   }
+}
