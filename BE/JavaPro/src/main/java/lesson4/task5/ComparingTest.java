31b83b327f1ffa67915b1cfdab052f49660d5ab8
ikar-zindo <plag.ikar@gmail.com>
2023-06-29 13:55:12 +0200

    update JavaPro + delete 1th + delete tel-ran-pro-june23

--- BE/JavaPro/src/main/java/lesson4/task5/ComparingTest.java
+++ BE/JavaPro/src/main/java/lesson4/task5/ComparingTest.java
@@ -1,2 +1,41 @@
-package lesson4.task5;public class ComparingTest {
+package lesson4.task5;
+
+import lesson4.task1.Cat;
+
+public class ComparingTest {
+   public static void main(String[] args) {
+
+      int x = 7;
+      int y = 7;
+      Integer integerX = 7;
+      Integer integerY = new Integer(7);
+
+      boolean compare = x == y;
+
+      System.out.println("Сравнение х и у: " + compare);
+
+      Cat cat1 = new Cat(3, "BLACK");
+      Cat cat2 = new Cat();
+      cat2.setColor("BLACK");
+      cat2.setAge(3);
+
+      compare = cat1 == cat2;
+      System.out.println("Сравнение двух котов: " + compare);
+
+      compare = cat1.equals(cat2);
+      System.out.println("Сравнение х и у: " + compare);
+
+      compare = x == integerX;
+      System.out.println("Сравнение int и Integer: " + compare);
+
+      compare = integerX == integerY;
+      System.out.println("Сравнение двух Integer: " + compare);
+
+      compare = cat1.equals(cat2);
+      System.out.println("Сравнение двух котов: " + compare);
+
+      int a = 50;
+      char ch = 'D';
+
+   }
 }
--- BE/JavaPro/src/main/java/lesson4/task7/Book.java
+++ BE/JavaPro/src/main/java/lesson4/task7/Book.java
@@ -1,2 +1,53 @@
-package lesson4.task7;public class Book {
+package lesson4.task7;
+
+import java.util.Objects;
+
+public class Book implements Cloneable{
+
+   private String name;
+   private Author author;
+
+   public Book(Book book) {
+      this.name = book.getName();
+      this.author = new Author(book.getAuthor());
+   }
+
+   public Book(String name, Author author) {
+      this.name = name;
+      this.author = author;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public Author getAuthor() {
+      return author;
+   }
+
+   public void setAuthor(Author author) {
+      this.author = author;
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Book book = (Book) o;
+      return Objects.equals(name, book.name) && Objects.equals(author, book.author);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(name, author);
+   }
+
+   @Override
+   protected Object clone() throws CloneNotSupportedException {
+      return super.clone();
+   }
 }
