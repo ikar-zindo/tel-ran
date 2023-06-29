cecafec832fcfce04018681dcc2c59a11f1e0cf2
ikar-zindo <plag.ikar@gmail.com>
2023-06-29 13:55:12 +0200

    update JavaPro + delete 1th + delete tel-ran-pro-june23

--- BE/JavaPro/src/main/java/lesson4/task2/Main.java
+++ BE/JavaPro/src/main/java/lesson4/task2/Main.java
@@ -1,2 +1,31 @@
-package lesson4.task2;public class Main {
+package lesson4.task2;
+
+import lesson4.task1.Cat;
+
+public class Main {
+   public static void main(String[] args) {
+
+      int value = 5;
+      Cat cat = new Cat();
+      cat.setAge(3);
+
+      System.out.println("Значения до вызова метода:");
+      System.out.println("Значение int - " + value);
+      System.out.println("Зничение возраста кота - " + cat.getAge());
+
+      changValue(value);
+      changValue(cat);
+
+      System.out.println("Значения после вызова метода:");
+      System.out.println("Значение int - " + value);
+      System.out.println("Зничение возраста кота - " + cat.getAge());
+   }
+
+   public static void changValue(int value) {
+      value++;
+   }
+
+   public static void changValue(Cat cat) {
+      cat.setAge(cat.getAge() + 1);
+   }
 }
--- BE/JavaPro/src/main/java/lesson4/task6/Main.java
+++ BE/JavaPro/src/main/java/lesson4/task6/Main.java
@@ -1,2 +1,12 @@
-package lesson4.task6;public class Main {
+package lesson4.task6;
+
+public class Main {
+   public static void main(String[] args) {
+
+      Book book1 = new Book("Java", "Gerbert Shildt");
+      Book book2 = book1;
+
+      book2.setName("Clean code");
+      System.out.println(book1.getName());
+   }
 }
