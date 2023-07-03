856ba3419279375c06cb6483bd28bbfbd34cc0fe
ikar-zindo <plag.ikar@gmail.com>
2023-07-03 14:42:14 +0200

    SS_Pro

--- BE/SS_Pro/src/main/java/org/example/collection/Test.java
+++ BE/SS_Pro/src/main/java/org/example/collection/Test.java
@@ -1,2 +1,13 @@
-package org.example.collection;public class Test {
+package org.example.collection;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test {
+   public static void main(String[] args) {
+
+      List<Integer> listLL = new LinkedList<>();
+
+
+   }
 }
