3798babcd7b7eceb75e00960122ea55daa77bd4b
ikar-zindo <plag.ikar@gmail.com>
2023-07-05 14:44:56 +0200

    poker_update_meth_cardDistribution()

--- BE/HoWkPro/src/main/java/GamePocker/Main.java
+++ BE/HoWkPro/src/main/java/GamePocker/Main.java
@@ -1,9 +1,9 @@
 package GamePocker;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
-        Game game = new Game("Poker");
+        Game game = new Game("Draw-poker");
         game.playing();
     }
 }
