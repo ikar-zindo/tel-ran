a59ab6ca2696f5669dfd2847d66be42ff8863565
ikar-zindo <plag.ikar@gmail.com>
2023-07-10 12:55:24 +0200

    lessons 7_iterator

---
+++ BE/JavaPro/src/main/java/lesson7/task1/IteratorTest.java
@@ -0,0 +1,46 @@
+package lesson7.task1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorTest {
+   public static void main(String[] args) {
+
+      List<String> fruits = new ArrayList<>();
+      fruits.add("Orange");
+      fruits.add("Banana");
+      fruits.add("Kiwi");
+      fruits.add("Apple");
+      fruits.add("Pineapple");
+      fruits.add("Apricot");
+
+      // Получаем объкт итератора для нашей коллекции
+      Iterator<String> iterator = fruits.iterator();
+
+      // Спрашиваем, а есть ли ещё объекты в нашей коллекции
+      System.out.println("Есть ли ещё объекты в коллекции - " + iterator.hasNext());
+
+      // Получаем следующий элемент из коллекции
+      System.out.println("Следующий элемент - " + iterator.next());
+
+      // Получаем осташиеся элементы коллекции при помощи цикла
+      while (iterator.hasNext()) {
+         System.out.print(iterator.next() + " ");
+      }
+
+      System.out.println();
+
+      for (int i = 0; i < fruits.size(); i++) {
+         String currentElement = fruits.get(i);
+         System.out.print(currentElement + " ");
+      }
+
+      System.out.println();
+
+      iterator = fruits.iterator();
+
+      while (iterator.hasNext())
+            System.out.print(iterator.next() + " ");
+   }
+}
---
+++ BE/JavaPro/src/main/java/lesson7/task5/SpliteratorTest.java
@@ -0,0 +1,34 @@
+package lesson7.task5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Spliterator;
+
+public class SpliteratorTest {
+   public static void main(String[] args) {
+
+      List<String> list = new ArrayList<>();
+
+      list.add("AAA"); // AAA -> AAAAAA
+      list.add("BBB");
+      list.add("CCC");
+      list.add("DDD");
+      list.add("EEE");
+      list.add("FFF");
+      list.add("GGG");
+      list.add("HHH");
+      list.add("III");
+
+      Spliterator<String> spliterator1 = list.spliterator();
+      Spliterator<String> spliterator2 = spliterator1.trySplit();
+      Spliterator<String> spliterator3 = spliterator2.trySplit();
+
+      spliterator1.forEachRemaining(x -> System.out.println("1й сплитеротор: " + x));
+      spliterator2.forEachRemaining(x -> System.out.println("2й сплитеротор: " + x));
+      spliterator3.forEachRemaining(x -> System.out.println("3й сплитеротор: " + x));
+
+      for (String current : list) {
+         System.out.print(current + current + "\n");
+      }
+   }
+}
