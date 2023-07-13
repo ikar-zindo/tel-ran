1b0aa493280fe0529369f5aa6fafb6d0a0d1286b
ikar-zindo <plag.ikar@gmail.com>
2023-07-13 12:55:53 +0200

    MyLinkedList

--- BE/HoWkPro/src/main/java/myLinkedList/MyLinkedList.java
+++ BE/HoWkPro/src/main/java/myLinkedList/MyLinkedList.java
@@ -1,2 +1,50 @@
-package myLinkedList;public class MyLinkedList {
+package myLinkedList;
+
+public class MyLinkedList {
+
+   private int size = 0;
+   private Node first;
+   private Node last;
+
+   /**
+    * Добавление элементы в начало списка
+    * @param value добавляеимое значение
+    */
+   public void addFirst(String value) {
+      Node current = new Node(value, first, last);
+      first = current;
+      size++;
+   }
+
+   /**
+    * Добавление элемента в конец списка
+    * @param value добавляемое значение
+    */
+   public void addLast(String value) {
+      if (size == 0) {
+         addFirst(value);
+         return;
+      }
+
+      Node current = last;
+      while ()
+
+      size++;
+   }
+
+   @Override
+   public String toString() {
+      if (size == 0) {
+         return "[]";
+      }
+      StringBuilder builder = new StringBuilder("[");
+      Node current = first;
+      while (current != null) {
+         builder.append(current.getValue()).append(", ");
+         current = current.getNext();
+      }
+      builder.setLength(builder.length() - 2);
+      builder.append("]");
+      return builder.toString();
+   }
 }
