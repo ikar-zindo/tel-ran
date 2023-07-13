3d99c302e455b431f03e3810283c3de02c59590b
ikar-zindo <plag.ikar@gmail.com>
2023-07-13 12:55:53 +0200

    MyLinkedList

--- BE/HoWkPro/src/main/java/myLinkedList/Main.java
+++ BE/HoWkPro/src/main/java/myLinkedList/Main.java
@@ -1,2 +1,14 @@
-package myLinkedList;public class Main {
+package myLinkedList;
+
+public class Main {
+   public static void main(String[] args) {
+
+      MyLinkedList list = new MyLinkedList();
+
+      list.addFirst("AAA");
+      list.addFirst("BBB");
+      list.addFirst("CCC");
+
+      System.out.println(list);
+   }
 }
--- BE/HoWkPro/src/main/java/myLinkedList/Node.java
+++ BE/HoWkPro/src/main/java/myLinkedList/Node.java
@@ -1,2 +1,52 @@
-package myLinkedList;public class Node {
+package myLinkedList;
+
+public class Node {
+
+   private String value;
+   private Node next;
+   private Node previous;
+
+   public Node(String value) {
+      this.value = value;
+   }
+
+   public Node(String value, Node next) {
+      this.value = value;
+      this.next = next;
+   }
+
+   public Node(Node previous, String value) {
+      this.value = value;
+      this.previous = previous;
+   }
+
+   public Node(String value, Node next, Node previous) {
+      this.value = value;
+      this.next = next;
+      this.previous = previous;
+   }
+
+   public String getValue() {
+      return value;
+   }
+
+   public void setValue(String value) {
+      this.value = value;
+   }
+
+   public Node getNext() {
+      return next;
+   }
+
+   public void setNext(Node next) {
+      this.next = next;
+   }
+
+   public Node getPrevious() {
+      return previous;
+   }
+
+   public void setPrevious(Node previous) {
+      this.previous = previous;
+   }
 }
