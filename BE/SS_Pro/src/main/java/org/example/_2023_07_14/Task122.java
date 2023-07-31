353a2fa81f4091846f059cbddccc3dbc57f17e59
ikar-zindo <plag.ikar@gmail.com>
2023-07-31 22:32:23 +0200

    SS_Pro _2023_07_14

--- BE/SS_Pro/src/main/java/org/example/_2023_07_14/Task122.java
+++ BE/SS_Pro/src/main/java/org/example/_2023_07_14/Task122.java
@@ -1,2 +1,46 @@
-package org.example._2023_07_14;public class Task122 {
+package org.example._2023_07_14;
+
+public class Task122 {
+   /**
+    * Сколько едениц встречается в двоичном числе
+    * 0 0 0 0 1 0 1 1 = 11
+    */
+   private static int bitCounter1(int n) {
+      int result = 0;
+      String str = Integer.toBinaryString(n);
+
+      for (int i = 0; i < str.length(); i++) {
+         if (str.charAt(i) == '1')
+            result++;
+      }
+      return result;
+   }
+
+   private static int bitCounter2(int n) { // 11---1011
+      int result = 0;
+
+      while (n != 0) { // 8 --- 1 0 0 0
+         n = n & (n - 1); // n = 8 & (8 -1)
+         /**
+          * 1 0 0 0
+          * 0 1 1 1
+          * 0 0 0 0
+          */
+         result++;
+      }
+      return result;
+   }
+
+   public static void main(String[] args) {
+      int a = 11;    // 0b00001011
+      int b = 255;   // 0b11111111
+
+      System.out.println(Integer.toBinaryString(a));
+      System.out.println(bitCounter1(a));
+
+      System.out.println(Integer.toBinaryString(b));
+      System.out.println(bitCounter2(b));
+
+      System.out.println(Integer.toBinaryString(10 & 8));
+   }
 }
