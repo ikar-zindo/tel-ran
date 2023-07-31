951c04671a712173bb1b0bf496d469f40290c5f6
ikar-zindo <plag.ikar@gmail.com>
2023-07-31 20:19:55 +0200

    Algorithms HoWk archUnpairedElement

--- Algorithms/AlgorithmsHoWk/src/main/java/HoWk1/Main.java
+++ Algorithms/AlgorithmsHoWk/src/main/java/HoWk1/Main.java
@@ -1,2 +1,31 @@
-package HoWk1;public class Main {
+package HoWk1;
+
+/**
+ * Дан отсортированный массив, в котором все элементы
+ * встречаются дважды (один за другим),
+ * а один элемент появляется только один раз.
+ * Написать метод для нахождения этого элемента.
+ * Определить асимптотическую сложность алгоритма.
+ */
+
+public class Main {
+   public static void main(String[] args) {
+
+      int[] arr = {5, 5, 6, 7, 7, 8, 8, 10, 10, 11, 11, 18, 18};
+
+      System.out.println(searchUnpairedElementMeth1(arr));
+   }
+
+   private static int searchUnpairedElementMeth1 (int[] arr) {
+      // Использую инсрументы задоженныв Java - побитовые операции
+      int x = 0;
+      for (int i : arr) {
+         x = x ^ i;
+      }
+      return x;
+   }
+
+   /**
+    * асимптотическую сложность алгоритма = О(n)
+    */
 }
