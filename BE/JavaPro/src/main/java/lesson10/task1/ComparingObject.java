f8fa7fe6a565b22b5c795f3c07620f91b1138fde
ikar-zindo <plag.ikar@gmail.com>
2023-08-02 19:14:36 +0200

    lessons 10 ComparingObject/Compare

--- BE/JavaPro/src/main/java/lesson10/task1/ComparingObject.java
+++ BE/JavaPro/src/main/java/lesson10/task1/ComparingObject.java
@@ -1,2 +1,25 @@
-package lesson10.task1;public class ComparingObject {
+package lesson10.task1;
+
+public class ComparingObject {
+   public static void main(String[] args) {
+
+      Cat cat1 = new Cat("BLACK", 5, 4.5);
+      Cat cat2 = new Cat("BLACK", 5, 4.5);
+
+      boolean result = cat1 == cat2;
+      System.out.println("Равны ли коты? := " + result);
+
+      Cat cat3 = cat2;
+      result = cat2 == cat3;
+      System.out.println("Равны ли коты 2 и 3? := " + result);
+
+      result = cat1.equals(cat2);
+      System.out.println("Равны ли коты 1 и 2 (.equals)? := " + result);
+
+      Cat cat4 = new Cat();
+      Cat cat5 = new Cat();
+
+      result = cat4.equals(cat5);
+      System.out.println("Равны ли коты 4 и 5? := " + result);
+   }
 }
