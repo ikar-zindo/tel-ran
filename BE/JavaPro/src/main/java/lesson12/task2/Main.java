696f5f66e51f918ed5a32d65d97b78c6e7f06b3e
ikar-zindo <plag.ikar@gmail.com>
2023-08-10 12:52:27 +0200

    JavaPro lesson12

---
+++ BE/JavaPro/src/main/java/lesson12/task2/Main.java
@@ -0,0 +1,68 @@
+package lesson12.task2;
+
+import lesson8.task2.Node;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+   public static void main(String[] args) {
+
+//        Найти маршрут из заданного списка билетов
+//        Учитывая список билетов, найти маршрут по порядку, используя данный список.
+//        Вход:
+//        «Berlin» -> «London»
+//        «Tokyo» -> «Seoul»
+//        «Mumbai» -> «Berlin»
+//        «Seoul» -> «Mumbai»
+//
+//        Выход:
+//        Tokyo -> Seoul, Seoul -> Mumbai, Mumbai -> Berlin, Berlin -> London
+
+      Map<String, String> tickets = new HashMap<>();
+      tickets.put("Berlin", "London");
+      tickets.put("Tokyo", "Seoul");
+      tickets.put("Mumbai", "Berlin");
+      tickets.put("Seoul", "Mumbai");
+
+      String startCity = getStartCity(tickets);
+
+      String route = getRoute(tickets, startCity);
+
+//        Выход:
+//        Tokyo -> Seoul, Seoul -> Mumbai, Mumbai -> Berlin, Berlin -> London
+      System.out.println(startCity);
+      System.out.println(route);
+   }
+
+   public static String getStartCity(Map<String, String> map) {
+
+      String startCity = "";
+
+      for (String city : map.keySet()) {
+         if (!map.containsValue(city)) {
+            startCity = city;
+         }
+      }
+
+      return startCity;
+   }
+
+   //        Tokyo -> Seoul, Seoul -> Mumbai, Mumbai -> Berlin, Berlin -> London
+   public static String getRoute(Map<String, String> map, String startCity) {
+
+      String destination;
+      StringBuilder sb = new StringBuilder();
+
+      while (map.get(startCity) != null) {
+         destination = map.get(startCity);
+         sb.append(startCity).append(" -> ").append(destination).append(", ");
+         startCity = destination;
+      }
+
+      sb.setLength(sb.length() - 2);
+      return sb.toString();
+   }
+}
