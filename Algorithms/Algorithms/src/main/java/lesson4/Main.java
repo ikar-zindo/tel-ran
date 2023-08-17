e597dfe921667ced25f27957fecfae39447b7394
ikar-zindo <plag.ikar@gmail.com>
2023-08-17 13:50:21 +0200

    Algorithms/RecursiveProgression/hanoiTower

---
+++ Algorithms/Algorithms/src/main/java/lesson4/Main.java
@@ -0,0 +1,13 @@
+package lesson4;
+
+public class Main {
+   public static void main(String[] args) {
+
+      int count = 0;
+      for (int i = 1; i <= 100; i++) {
+         count += i;
+      }
+
+      System.out.println(count);
+   }
+}
