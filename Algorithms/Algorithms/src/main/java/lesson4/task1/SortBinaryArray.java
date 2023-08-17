cd14cca27fe29ce81779f8244c62ca52fce9cf51
ikar-zindo <plag.ikar@gmail.com>
2023-08-17 13:50:21 +0200

    Algorithms/RecursiveProgression/hanoiTower

---
+++ Algorithms/Algorithms/src/main/java/lesson4/task1/SortBinaryArray.java
@@ -0,0 +1,26 @@
+package lesson4.task1;
+
+import java.util.Arrays;
+
+public class SortBinaryArray {
+   public static void main(String[] args) {
+      int[] arr = {0, 1, 1, 0, 1, 1, 0, 0, 0};
+      sort(arr);
+
+      System.out.println(Arrays.toString(arr));
+   }
+
+
+   public static void sort(int[] arr) {
+
+      int count = 0;
+
+      for (int i : arr) {
+         if (i == 0) count++;
+      }
+      for (int i = 0; i < arr.length; i++) {
+         if (i < count) arr[i] = 0;
+         else arr[i] = 1;
+      }
+   }
+}
