f7a4f68d550d83c1e59815ea4442db2d52eac5cd
ikar-zindo <plag.ikar@gmail.com>
2023-08-22 13:41:13 +0200

    upload test/MyIterator.java

--- BE/JavaPro/src/main/java/test/Main.java
+++ BE/JavaPro/src/main/java/test/Main.java
@@ -1,2 +1,30 @@
-package test;public class Main {
+package test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+   public static void main(String[] args) {
+
+      // Существует список чисел
+      // Реализовать итератор, который из переданого списка возвращает
+      // только чётные числа
+
+      List<Integer> numbers = new ArrayList<>();
+
+      numbers.add(1);
+      numbers.add(2);
+      numbers.add(3);
+      numbers.add(4);
+      numbers.add(5);
+      numbers.add(6);
+      numbers.add(7);
+
+      Iterator<Integer> iterator = new MyIterator(numbers);
+
+      while (iterator.hasNext()) {
+         System.out.print(iterator.next() + " "); // 2 4 6
+      }
+   }
 }
