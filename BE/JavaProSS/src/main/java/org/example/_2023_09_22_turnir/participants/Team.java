84358635997a9822ef3689c0232f33dda9bc25a2
ikar-zindo <plag.ikar@gmail.com>
2023-10-22 20:28:05 +0200

    JavaProSS/Generics/Participants/Adult,Student,Pupil

---
+++ BE/JavaProSS/src/main/java/org/example/_2023_09_22_turnir/participants/Team.java
@@ -0,0 +1,37 @@
+package org.example._2023_09_22_turnir.participants;
+
+import java.util.*;
+
+public class Team<T extends Participant> {
+
+   private final String name;
+   private final List<T> participantList = new ArrayList<>();
+
+   public Team(String name) {
+      this.name = name;
+   }
+
+   public void addNewParticipant(T participant) {
+      participantList.add(participant);
+      System.out.println("Participant: " + participant + " was added to the team [" + name + "]");
+   }
+
+   public String playWith(Team<? extends Participant> team) {
+      // Выводит название команды победителя игры и возвращает её имя
+      String winnerName;
+      Random random = new Random();
+
+      int i = random.nextInt(2);
+
+      if (i == 0) {
+         winnerName = this.name;
+      } else {
+         winnerName = team.name;
+      }
+
+      System.out.println(winnerName);
+
+      return winnerName;
+   }
+
+}
