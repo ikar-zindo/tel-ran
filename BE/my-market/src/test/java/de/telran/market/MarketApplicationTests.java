4894c0b13418edc6e91a3bce8c7b3a69694d18ca
ikar-zindo <plag.ikar@gmail.com>
2023-10-30 18:34:11 +0100

    without comments

---
+++ BE/my-market/src/test/java/de/telran/market/MarketApplicationTests.java
@@ -0,0 +1,15 @@
+package de.telran.market;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
