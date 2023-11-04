be3f307db0ba62b83867a8844e182faa769a7c1b
ikar-zindo <plag.ikar@gmail.com>
2023-11-04 11:15:45 +0100

    shop: refactoring/add comments

--- BE/g_280323_m_be_shop/src/main/java/de/telran/g_280323_m_be_shop/repository/mysql/MySqlProductRepository.java
+++ BE/g_280323_m_be_shop/src/main/java/de/telran/g_280323_m_be_shop/repository/mysql/MySqlProductRepository.java
@@ -11,11 +11,22 @@ import java.util.List;
 import java.util.Locale;
 
 import static de.telran.g_280323_m_be_shop.domain.database.MySqlConnector.getConnection;
+
+/**
+ * Репозиторий продуктов.
+ * Содержит сформированные SQL запросы в методах для поиска отображения всех продуктов,
+ * поиску, а так же добавление, удаление продуктов из БД.
+ */
 public class MySqlProductRepository implements ProductRepository {
 
    private final String ID = "product_id";
    private final String NAME = "name";
    private final String PRICE = "price";
+
+   /**
+    * Получение всех продуктов.
+    * @return список всех продуктов, хранящихся в БД.
+    */
    @Override
    public List<Product> getAll() {
       try (Connection connection = getConnection()) {
@@ -38,6 +49,11 @@ public class MySqlProductRepository implements ProductRepository {
       }
    }
 
+   /**
+    * Получение продукта по идентификатору.
+    * @param id    идентификатор продукта.
+    * @return      продукт, имеющий переданный идентификатор.
+    */
    @Override
    public Product getById(int id) {
       try (Connection connection = getConnection()) {
@@ -58,6 +74,11 @@ public class MySqlProductRepository implements ProductRepository {
       }
    }
 
+   /**
+    * Добавление продукта
+    * @param name    наименование продукта, добавляемого в БД
+    * @param price   цена продукта, добавляемого в БД
+    */
    @Override
    public void add(String name, double price) {
       try (Connection connection = getConnection()) {
@@ -68,6 +89,10 @@ public class MySqlProductRepository implements ProductRepository {
       }
    }
 
+   /**
+    * Удаление продукта.
+    * @param id идентификатор удаляемого из БД продукта.
+    */
    @Override
    public void delete(int id) {
       try (Connection connection = getConnection()) {
