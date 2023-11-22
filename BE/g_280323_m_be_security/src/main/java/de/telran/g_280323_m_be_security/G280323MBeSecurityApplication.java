29c80719fa14746e1cc8133a39f7dd9a626ee07c
Ivan Bukrieiev <plag.ikar@gmail.com>
2023-11-22 13:45:36 +0100

    security-proj

---
+++ BE/g_280323_m_be_security/src/main/java/de/telran/g_280323_m_be_security/G280323MBeSecurityApplication.java
@@ -0,0 +1,13 @@
+package de.telran.g_280323_m_be_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G280323MBeSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G280323MBeSecurityApplication.class, args);
+	}
+
+}
---
+++ BE/g_280323_m_be_security/src/test/java/de/telran/g_280323_m_be_security/G280323MBeSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telran.g_280323_m_be_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G280323MBeSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
