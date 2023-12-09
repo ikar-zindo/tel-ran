7557c0ca3fbce0b0fe6b17c58df0c51fcea405c6
Ivan Bukrieiev <plag.ikar@gmail.com>
2023-12-09 20:28:27 +0100

    g_280323_m_be_dto

---
+++ BE/g_280323_m_be_dto/src/main/java/de/telran/g_280323_m_be_dto/G280323MBeDtoApplication.java
@@ -0,0 +1,13 @@
+package de.telran.g_280323_m_be_dto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G280323MBeDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G280323MBeDtoApplication.class, args);
+	}
+
+}
