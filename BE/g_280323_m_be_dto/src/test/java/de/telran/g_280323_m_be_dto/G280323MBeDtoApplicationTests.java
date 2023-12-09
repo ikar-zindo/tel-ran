2840db0632853cae2b121a748cf853b0dac5567b
Ivan Bukrieiev <plag.ikar@gmail.com>
2023-12-09 20:28:27 +0100

    g_280323_m_be_dto

---
+++ BE/g_280323_m_be_dto/src/test/java/de/telran/g_280323_m_be_dto/G280323MBeDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telran.g_280323_m_be_dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G280323MBeDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
